812819d7338c0f77fe031cd184ec12dee6aa4be5
Furkan Deniz <76913129+asabibey@users.noreply.github.com>
2022-07-24 03:43:44 +0300

    Add files via upload

---
+++ furkandeniz.sql
@@ -0,0 +1,14 @@
+1.SORU
+-- SELECT title , description FROM film
+2.SORU
+-- SELECT * FROM film
+-- WHERE  length>60 AND length<75 
+3.SORU     
+-- WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99 ;
+4.SORU 
+-- SELECT * FROM customer     
+-- WHERE first_name = 'Mary' ; last_name = (Smith)
+5.SORU
+-- SELECT * FROM film
+-- WHERE NOT (length >50 OR rental_rate = 2.99 OR rental_rate = 4.99) ;
+
